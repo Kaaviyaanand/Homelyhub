79387dd9ae95c459670405aef247b65c9f26f578
Kaaviya sri <150776334+Kaaviyaanand@users.noreply.github.com>
2025-04-24 14:36:06 +0530

    Update PropertyDetails-action.js

--- Frontend/src/Store/PropertyDetails/PropertyDetails-action.js
+++ Frontend/src/Store/PropertyDetails/PropertyDetails-action.js
@@ -5,7 +5,7 @@ import { propertyDetailsAction } from "./PropertyDetails-slice";
 export const getPropertyDetails=(id)=>async(dispatch)=>{
     try{
     dispatch(propertyDetailsAction.getListRequest());
-     const response=await axios(`/api/v1/rent/listing/${id}`);
+     const response=await axios(`https://backennnd-1.onrender.com/api/v1/rent/listing/${id}`);
      if(! response){
         throw new Error("could not fetch any property details")
      }
